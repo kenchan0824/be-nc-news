3840a088b3c5cfef19f5f88af024135e5fb437a7
kenchan0824 <kenchan0824@gmail.com>
2023-03-30 15:42:00 +0100

    complete 10-get-users

---
+++ controllers/users.controllers.js
@@ -0,0 +1,11 @@
+const { fetchUsers } = require("../models/users.models");
+
+function getUsers(req, res, next) {
+  fetchUsers()
+    .then((users) => {
+      res.status(200).send({ users });
+    })
+    .catch(next);
+}
+
+module.exports = { getUsers };
